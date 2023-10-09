adaf821a4bcfdff0a073d3e292fb3efd16292af0
ndominik12 <113335133+ndominik12@users.noreply.github.com>
2023-10-09 13:08:54 +0200

    Add files via upload

---
+++ Szamonkeres_01/Szamonkeres_001.py
@@ -0,0 +1,14 @@
+print("Nagy Dominik")
+jegy = int(input("Kérek egy jegyet 1-5:"))
+if jegy == 5:
+    print("a jegyed 5 jeles")
+elif jegy == 4:
+    print("a jegyed 4 jeles")
+elif jegy == 3:
+    print("a jegyed 3 közepes")
+elif jegy == 2:
+    print("a jegyed 2 elégséges")
+elif jegy == 1:
+    print("a jegyed 1 elégtelen")
+else jegy <= 6:
+    print("a jegyed")
