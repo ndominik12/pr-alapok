59bcf42995ad85d7c8a45da409c7aa8416b40528
ndominik12 <113335133+ndominik12@users.noreply.github.com>
2023-11-06 13:05:42 +0100

    Add files via upload

---
+++ doga2/doga2.py
@@ -0,0 +1,8 @@
+#Nagy Dominik
+szam = int(input('Kérek egy számot'))
+if szam < 0:
+    print(f'A megadott szám {szam} negatív')
+elif szam > 0:
+    print(f'A megadott szám {szam} pozitív')
+
+    
