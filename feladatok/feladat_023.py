f8c0ac63470074827156c1b55473e579855928c2
ndominik12 <113335133+ndominik12@users.noreply.github.com>
2023-11-27 14:16:44 +0100

    Update feladat_023.py

--- feladatok/feladat_023.py
+++ feladatok/feladat_023.py
@@ -1,11 +1,34 @@
-# feladat_023
-honapok = ['január', 'február', 'március', 'április', 'május']
+honapok = ['január', 'február','március', 'április']
 
+  # lista kiíratása
 print(honapok)
-print(('I').join(honapok))
 
+  # join(): a lista elemeit egy sztringgé fűzi össze
+  # a megadott elválasztó karakterekkel tagolva
+print(', '.join(honapok))
+  
+  # lista hossza: len( )
 print(len(honapok))
+  
+  # a 0. indexű az első elem
 print(honapok[0])
-print(f'A lista utolsó legnagyobb indexe: ')
-
-#a len a hossz függvényt jelenti
+  
+  # jelen esetben a 3. indexű az utolsó elem
+print(honapok[3])
+  
+  # túlindexelés, hibát okoz!
+  # ebben a listában az utolsó elem indexe: 3
+  # print(honapok[4]) <- HIBÁS!!!
+  
+  # az 1-es és 2-es indexű elemek kiíratása
+print(honapok[1:3])
+  
+  # az elejétől a 2-es indexű elemmel bezárólag
+print(honapok[:3])
+  
+  # 2-es indexű elemtől a végéig
+print(honapok[2:])
+  
+  # hátulról az első, vagyis az utolsó elem
+print(honapok[-1])      
+  
